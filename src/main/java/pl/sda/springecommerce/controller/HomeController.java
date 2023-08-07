0419f956a31b4f59d0449b679b8f4ee1fbf089ec
bartosz.dziedzic <bartekkdziedzic@gmail.com>
2023-08-07 11:39:08 +0200

    Thymeleaf for a single unit (without image url)

--- src/main/java/pl/sda/springecommerce/controller/HomeController.java
+++ src/main/java/pl/sda/springecommerce/controller/HomeController.java
@@ -1,15 +1,21 @@
 package pl.sda.springecommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.springecommerce.model.Product;
+
+import java.math.BigDecimal;
 
 @Controller
 public class HomeController {
 
 
     @GetMapping("/")
-    public String home(){
-        return "home.jsp";
+    public String home(Model model){
+        model.addAttribute("product", new Product("Piggy Bank",new BigDecimal(7.50),"https://m.media-amazon.com/images/I/51ckv2myWXL.jpg"));
+        // new Product("Piggy Bank",new BigDecimal(7.50),"https://m.media-amazon.com/images/I/51ckv2myWXL.jpg");
+        return "home";
     }
 
 
