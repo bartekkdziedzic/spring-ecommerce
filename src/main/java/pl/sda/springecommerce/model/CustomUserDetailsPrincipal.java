b874108d0f6014e8b3c92dcf3da9d8110db581ef
bartekdziedzic <bartekkdziedzic@gmail.com>
2023-08-24 21:07:54 +0200

    - working login feature added - header refactor

--- src/main/java/pl/sda/springecommerce/model/CustomUserDetailsPrincipal.java
+++ src/main/java/pl/sda/springecommerce/model/CustomUserDetailsPrincipal.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class CustomUserDetailsPrincipal implements UserDetails {
 
@@ -15,36 +16,36 @@ public class CustomUserDetailsPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return  List.of();
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
